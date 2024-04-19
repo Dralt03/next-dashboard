8bcb1f820db518adc7d5bc1e904b48e387aae541
Sauren Sharma <sharmasaurenb@gmail.com>
2024-04-19 18:44:21 +0530

    First Message

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -2,9 +2,10 @@ import { Card } from "../../ui/dashboard/cards"
 import RevenueChart from "../../ui/dashboard/revenue-chart"
 import LatestInvoices from "../../ui/dashboard/latest-invoices"
 import { lusitana } from "../../ui/fonts"
-import { fetchLatestInvoices, fetchCardData } from "../../lib/data"
+import { fetchRevenue, fetchLatestInvoices, fetchCardData } from "../../lib/data"
 
 export default async function Page(){
+    const revenue = await fetchRevenue();
     const latestInvoices = await fetchLatestInvoices(); 
     const {totalPaidInvoices,
             totalPendingInvoices,
