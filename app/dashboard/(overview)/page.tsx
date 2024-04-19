382159e90018d59b324b4851b318d6b634a47b80
Sauren Sharma <sharmasaurenb@gmail.com>
2024-04-19 19:08:24 +0530

    First Message

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,8 +1,8 @@
-import { Card } from "../../ui/dashboard/cards"
-import RevenueChart from "../../ui/dashboard/revenue-chart"
-import LatestInvoices from "../../ui/dashboard/latest-invoices"
-import { lusitana } from "../../ui/fonts"
-import { fetchLatestInvoices, fetchCardData } from "../../lib/data"
+import { Card } from "@/app/ui/dashboard/cards"
+import RevenueChart from "@/app/ui/dashboard/revenue-chart"
+import LatestInvoices from "@/app/ui/dashboard/latest-invoices"
+import { lusitana } from "@/app/ui/fonts"
+import { fetchLatestInvoices, fetchCardData } from "@/app/lib/data"
 import { Suspense } from "react"
 import { RevenueChartSkeleton } from "@/app/ui/skeletons"
 
